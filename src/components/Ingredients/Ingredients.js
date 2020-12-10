0d7963e8a4a70d4fb737346530a395f58e868dd9
Jesuloba <jesulobajohn@gmail.com>
2020-12-10 23:13:45 +0100

    Adding Loading Spinner

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,9 +4,12 @@ import axios from "axios";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 const Ingredients = () => {
 	const [userIngredients, setUserIngredients] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState();
 
 	const filteredIngredientHandler = useCallback((filteredIngredients) => {
 		setUserIngredients(filteredIngredients);
@@ -17,12 +20,14 @@ const Ingredients = () => {
 	}, [userIngredients]);
 
 	const addIngredientHandler = (ingredient) => {
+		setIsLoading(true);
 		axios
 			.post(
 				"https://intro-hooks-default-rtdb.firebaseio.com/ingredient.json",
 				ingredient
 			)
 			.then((response) => {
+				setIsLoading(false);
 				return response.data.name;
 			})
 			.then((name) => {
@@ -34,22 +39,37 @@ const Ingredients = () => {
 	};
 
 	const removeIngredientHandler = (ingredientId) => {
+		setIsLoading(true);
 		axios
 			.delete(
 				`https://intro-hooks-default-rtdb.firebaseio.com/ingredient/${ingredientId}.json`
 			)
 			.then((response) => {
+				setIsLoading(false);
 				setUserIngredients((prevIngredients) =>
 					prevIngredients.filter(
 						(ingredient) => ingredient.id !== ingredientId
 					)
 				);
+			})
+			.catch((error) => {
+				setError(error.message);
+				setIsLoading(false);
 			});
+	};
+
+	const clearError = () => {
+		setError(null);
 	};
 
 	return (
 		<div className="App">
-			<IngredientForm onAddIngredient={addIngredientHandler} />
+			{error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
+			<IngredientForm
+				onAddIngredient={addIngredientHandler}
+				loading={isLoading}
+			/>
 
 			<section>
 				<Search onLoadIngredient={filteredIngredientHandler} />
