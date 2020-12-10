90a1cf13612dff37faf0b6fa53af46574e9b6836
Jesuloba <jesulobajohn@gmail.com>
2020-12-10 07:56:43 +0100

    Added useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import IngredientForm from "./IngredientForm";
@@ -7,6 +7,25 @@ import Search from "./Search";
 
 const Ingredients = () => {
 	const [ingredients, setIngredients] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				"https://intro-hooks-default-rtdb.firebaseio.com/ingredient.json"
+			)
+			.then((response) => {
+				const loadedIngredients = [];
+				for (const key in response.data) {
+					loadedIngredients.push({
+						id: key,
+						title: response.data[key].title,
+						amount: response.data[key].amount,
+					});
+				}
+
+				setIngredients(loadedIngredients);
+			});
+	}, []);
 
 	const addIngredientHandler = (ingredient) => {
 		axios
