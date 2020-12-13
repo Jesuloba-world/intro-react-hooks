40e83c40b078afa53809d719567d989d5fb194bd
Jesuloba <jesulobajohn@gmail.com>
2020-12-13 23:46:51 +0100

    using the custom hook in the search component

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import axios from "axios";
+import useHttp from "../../hooks/http";
+import ErrorModal from "../UI/ErrorModal";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -8,6 +9,7 @@ const Search = React.memo((props) => {
 	const { onLoadIngredient } = props;
 	const [filter, setFilter] = useState("");
 	const inputRef = useRef();
+	const { isLoading, data, error, sendRequest, clear } = useHttp();
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
@@ -16,34 +18,40 @@ const Search = React.memo((props) => {
 					filter.length === 0
 						? ""
 						: `?orderBy="title"&equalTo="${filter}"`;
-				axios
-					.get(
-						"https://intro-hooks-default-rtdb.firebaseio.com/ingredient.json" +
-							query
-					)
-					.then((response) => {
-						const loadedIngredients = [];
-						for (const key in response.data) {
-							loadedIngredients.push({
-								id: key,
-								title: response.data[key].title,
-								amount: response.data[key].amount,
-							});
-						}
-						onLoadIngredient(loadedIngredients);
-					});
+
+				sendRequest(
+					"https://intro-hooks-default-rtdb.firebaseio.com/ingredient.json" +
+						query,
+					"get"
+				);
 			}
 		}, 500);
 		return () => {
 			clearTimeout(timer);
 		};
-	}, [filter, onLoadIngredient, inputRef]);
+	}, [filter, inputRef, sendRequest]);
+
+	useEffect(() => {
+		if (!isLoading && !error && data) {
+			const loadedIngredients = [];
+			for (const key in data) {
+				loadedIngredients.push({
+					id: key,
+					title: data[key].title,
+					amount: data[key].amount,
+				});
+			}
+			onLoadIngredient(loadedIngredients);
+		}
+	}, [data, isLoading, error, onLoadIngredient]);
 
 	return (
 		<section className="search">
+			{error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
 			<Card>
 				<div className="search-input">
 					<label>Filter by Title</label>
+					{isLoading && <span>Loading...</span>}
 					<input
 						ref={inputRef}
 						type="text"
